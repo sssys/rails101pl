ba8088a55bf91ba867145066eee497ded1919c97
s <457492854@qq.com>
2017-06-17 10:33:07 +0800

    use self delimit helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def render_movie_description(movie)
+    simple_format(movie.description)
+  end
+
+  def render_review_content(review)
+    simple_format(review.content)
+  end
 end
